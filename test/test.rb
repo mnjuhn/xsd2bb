2eb6bfa74ebfcc4835aff12599f508e0ce1987f9
Joel VanderWerf <vjoel@users.sourceforge.net>
2012-03-09 14:39:50 -0800

    added test:update_expected task

--- test/test.rb
+++ test/test.rb
@@ -10,6 +10,18 @@ require 'xsd2bb'
 # Then it recursively compares the dirs.
 # Note that the input files are in git, but the outputs are not.
 
+if ARGV.delete "--update-expected"
+  Dir.chdir "tmp" rescue exit
+  
+  Dir.glob("*/output").each do |output_dir|
+    expected_dir = output_dir.sub("output", "expected")
+    FileUtils.rm_rf expected_dir
+    FileUtils.cp_r output_dir, expected_dir
+  end
+
+  exit
+end
+
 xsd_file = ARGV[0]
 
 schema_name = File.basename(xsd_file, ".xsd")
@@ -20,7 +32,7 @@ out_dir = "tmp/#{schema_name}/output"
 FileUtils.makedirs in_dir
 FileUtils.makedirs out_dir
 
-system "rm -rf #{out_dir}"
+FileUtils.rm_rf out_dir
 
 begin
   gen_opts = {
